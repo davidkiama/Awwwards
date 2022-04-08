0d5af4ddfec39b25307745341806fccf36b47a0d
davidkiama <davidkiama98@gmail.com>
2022-04-08 14:46:27 +0300

    Able to render the authentication pages

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members.apps.MembersConfig',
     'home.apps.HomeConfig',
 ]
 
--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(
+        upload_to='profile_pics', default='profile_pics/default.jpg')
+
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return (self.user.username)
+
+    def save_user(self):
+        self.save()
