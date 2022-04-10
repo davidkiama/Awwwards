2290bd0a6ffa84afa8ee9ab7a0346de0be56ad07
davidkiama <davidkiama98@gmail.com>
2022-04-10 15:02:00 +0300

    Able to submit new project

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Project
 
 # Create your views here.
 
@@ -15,7 +15,6 @@ def profile(request, pk):
 
 
 def edit_profile(request):
-
     user = request.user
     # Test if profile is created
     try:
@@ -45,4 +44,17 @@ def edit_profile(request):
 
 
 def submit_project(request):
+    user = request.user
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        link = request.POST['link']
+        image = request.FILES['image']
+        category = request.POST['category']
+        rating = 0
+
+        project = Project(title=title, description=description,
+                          link=link, image=image, category=category, rating=rating, user=user)
+        project.save_project()
+
     return render(request, 'submit_project.html', {'title': 'Submit Project'})
