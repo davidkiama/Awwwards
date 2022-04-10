55fa6617a9e0b9f61b7b35813e9d2c038d75282c
davidkiama <davidkiama98@gmail.com>
2022-04-10 10:17:04 +0300

    Redirect to profile page

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user/<pk>/', views.profile, name='profile'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html', {'title': 'Home'})
+
+
+def profile(request, pk):
+    profile = User.objects.get(id=pk)
+    return render(request, 'profile.html', {'title': 'Profile', 'profile': profile})
