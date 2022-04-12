f661cc689af206a731a2c2e91277397aba4198a7
davidkiama <davidkiama98@gmail.com>
2022-04-12 11:33:04 +0300

    Switched to cloudinary to store images

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -2,6 +2,11 @@
 import os
 from pathlib import Path
 
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,6 +143,13 @@ LOGOUT_REDIRECT_URL = 'home'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+cloudinary.config(
+    cloud_name="dvua6fhuc",
+    api_key="242318296529564",
+    api_secret="0l6qjwjrmEFXuT__sbCZgLm1Xyc"
+)
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
@@ -24,7 +26,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     link = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='projects')
+    image = CloudinaryField('image')
     date_posted = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=100)
     rating = models.DecimalField(default=0, decimal_places=1, max_digits=3)
